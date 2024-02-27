2df3619007488325d0995646d6027ce8b680cd12
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-27 10:46:24 +0530

    header ui developing done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,21 @@
+import Menu from "../Images/Menu.png"
+import Cart from "../Images/Cart.png"
+
 export const Header = () => {
     return (
-        <></>
+        <header className='w-full h-20 flex justify-between'>
+            <div className='flex gap-10 items-center'>
+                <div className='w-20 h-full bg-black flex items-center justify-center'>
+                    <img src={Menu} className='w-8'/>
+                </div>
+
+                <h1 className='text-[30px] font-bold'>TrendWear.</h1>
+            </div>
+
+            <div className='w-32 h-full bg-black flex gap-3 items-center justify-center'>
+                <img src={Cart} className='w-6 h-6'/>
+                <h1 className='text-white text-[12px]'><span className='font-bold text-[18px]'>2</span> in Bag</h1>
+            </div>
+        </header>
     )
 }
